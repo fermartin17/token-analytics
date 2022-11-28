ac05e2752b488e022e08fe0fe1bd905837fb78d3
Fer Martin <feer.1711@gmail.com>
2022-11-28 12:46:00 -0300

    enable cors

--- backend/dashboard/dashboard-service/src/main.ts
+++ backend/dashboard/dashboard-service/src/main.ts
@@ -1,8 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { json } from 'express';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
   await app.listen(3000);
+
+  app.use(json());
+  app.enableCors({
+    origin: '*',
+    methods: ['POST', 'PUT', 'DELETE', 'GET'],
+    exposedHeaders: ['resource-id'],
+  });
 }
 bootstrap();

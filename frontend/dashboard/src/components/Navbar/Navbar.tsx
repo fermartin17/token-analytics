592f0d0f10138ddd2eea8ce4cb518c7b4172169e
Fer Martin <feer.1711@gmail.com>
2022-11-30 21:56:34 -0300

    navbar full responsive

--- frontend/dashboard/src/components/Navbar/Navbar.tsx
+++ frontend/dashboard/src/components/Navbar/Navbar.tsx
@@ -5,18 +5,16 @@ import "./Navbar.scss";
 
 import {FC} from "react";
 import {Grid} from "@mui/material";
-import SearchIcon from "@mui/icons-material/Search";
 
 
 export const Navbar: FC<NavbarInterface> = ({title}) => {
 
   return (
     <Grid container xs={12} sm={12} md={12} className={"navbar"}>
-      <Grid item xs={12} sm={6} md={6} className={"navbar-text"}>
+      <Grid xs={12} sm={6} md={6} className={"navbar-text"}>
         {title}
       </Grid>
-      <Grid item xs={12} sm={6} md={6} className={"navbar-search"}>
-        <SearchIcon/>
+      <Grid xs={12} sm={6} md={6} className={"navbar-search"}>
         <input className={"search-input"} type="text" placeholder="Search..." />
       </Grid>
     </Grid>

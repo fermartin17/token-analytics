d2a9a5c2091d167d81430ce129ecce486373a19d
Fer Martin <feer.1711@gmail.com>
2022-12-06 09:15:20 -0300

    delete unnecessary routes

--- frontend/dashboard/src/App.js
+++ frontend/dashboard/src/App.js
@@ -1,14 +1,7 @@
 import React from "react";
 
 import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
-import {Card} from "./components/Card/Card";
-import {CardDeck} from "./components/CardDeck/CardDeck";
-import {Navbar} from "./components/Navbar/Navbar";
-import {Sidebar} from "./components/Sidebar/Sidebar";
-import {CardSection} from"./pages/dashboard/sections/CardSection";
 import {Dashboard} from "./pages/dashboard/Dashboard";
-import {ChartSection} from "./pages/dashboard/sections/ChartSection";
-import {LineChart} from "./components/LineChart/LineChart";
 
 import Axios from "axios";
 
@@ -18,14 +11,7 @@ const App = () => {
   return (
     <Router>
       <Routes>
-        <Route path={"/card"} element={<Card/>}/>
-        <Route path={"/deck"} element={<CardDeck/>}/>
-        <Route path={"/navbar"} element={<Navbar/>}/>
-        <Route path={"/sidebar"} element={<Sidebar/>}/>
-        <Route path={"/section"} element={<CardSection/>}/>
         <Route path={"/dashboard/pairs/:pairToken"} element={<Dashboard/>}/>
-        <Route path={"/chart"} element={<LineChart/>}/>
-        <Route path={"/chart-section"} element={<ChartSection/>}/>
       </Routes>
     </Router>
   );
